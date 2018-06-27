fde74fe10d70070e0df9a9dbafe1d74bc7b2ea9f
besik007 <west.south190011@gmail.com>
2018-06-27 19:31:34 +0700

    fixed reducer bug

--- src/core/modules/submission/reducer.js
+++ src/core/modules/submission/reducer.js
@@ -3,14 +3,14 @@ import {
 } from './actions';
 import { initialState } from '../initialState';
 
-export default function auth(state = initialState.auth, action = {}) {
+export default function submission(state = initialState.submission, action = {}) {
   const { type, payload } = action;
 
   switch (type) {
     case SUBMISSION_REQUEST_SUCCESS: {
       return {
         ...state,
-        submissionList: payload.list
+        list: payload.list
       }
       break;
     }
